e97e28c5abd30b018ccf4143fc3aea368d1619cf
Kevin Sivabalan <jbosshellokitty@gmail.com>
2013-09-16 13:42:06 -0400

    policy

--- api/policies/authenticated.js
+++ api/policies/authenticated.js
@@ -4,12 +4,13 @@
 module.exports = function (req, res, ok) {
 
   // User is allowed, proceed to controller
-  if (req.session.authenticated) {
+  if (req.isAuthenticated) {
     return ok();
   }
 
   // User is not allowed
   else {
-    return res.send("You are not permitted to perform this action.", 403);
+  	res.redirect('/login');  
+    res.send("You are not permitted to perform this action.", 403);
   }
 };
