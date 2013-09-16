f3af24cb76842b77d44a94c38d8977c8abf01708
Kevin Sivabalan <jbosshellokitty@gmail.com>
2013-09-16 13:31:58 -0400

    adding login lout auth logic

--- api/controllers/SessionController.js
+++ api/controllers/SessionController.js
@@ -4,14 +4,34 @@
  * @module		:: Controller
  * @description	:: Contains logic for handling requests.
  */
+var passport = require('passport');
 
 module.exports = {
+	'login': function (req,res){
+		res.view();
+	},
 
-  /* e.g.
-  sayHello: function (req, res) {
-    res.send('hello world!');
-  }
-  */
+	'process': function(res,req){
+		passport.authenticate('local', function (err,usr,info){
+			if ((err)||(!usr)){
+				return res.send({
+					message: 'Login Fail'
+				});
+				res.send(err);
+			}
+			req.logIn(usr, function(err){
+				if(err) res.send(err);
+				return res.send({
+					message: 'Login Successful'
+				});
+			});
+		})(req,res);
+	},
+
+	'logout': function(res,req){
+		req.logout();
+		res.send('Logout Successful');
+	}
   
 
 };
--- api/models/User.js
+++ api/models/User.js
@@ -5,15 +5,39 @@
  * @description :: A short summary of how this model works and what it represents.
  *
  */
+var bcrypt = require('bcrypt');
+
 
 module.exports = {
 
   attributes: {
-  	
-  	/* e.g.
-  	nickname: 'string'
-  	*/
-    
-  }
+  	username:{
+  		type: 'string'
+  		required: true,
+  		unique: true
+  	},
+  	password:{
+  		type: 'string',
+  		required: true
+  	},
+  	toJSON: function() {
+  		var obj = this.toObject();
+  		delete obj.password;
+  		return obj;
+  	}
+  },
 
+  beforeCreate: function (usr, cb){
+  	bcrypt.genSalt(10, function (err, salt){
+  		bcrypt.hash(usr.password, salt, function  (err, hash) {
+  			if(err){
+  				console.log(err);
+  				cb(err);
+  			} else {
+  				user.password = hash;
+  				cb(null, usr);
+  			}
+  		});
+  	});
+  }
 };
