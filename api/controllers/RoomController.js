619cd59d95f26eee4bf2fcb41ec5c58c1ad1f3b9
Kevin Sivabalan <jbosshellokitty@gmail.com>
2013-09-16 15:34:46 -0400

    creation of virtual rooms

--- api/controllers/RoomController.js
+++ api/controllers/RoomController.js
@@ -7,11 +7,35 @@
 
 module.exports = {
 
-  index: function(req,res,next){
+  'index': function(req,res,next){
   	Room.find( function foundRooms(err,rooms){
   		if(err) return console.log(err);
-  		else { res.send(rooms) }
-  	})
-  }
+  		else { res.view({ rooms: rooms }); }
+  	});
+  },
+
+  'new': function(req,res){
+  		res.view()
+  },
+
+  'create': function(req,res,next){
+  	Room.create(req.params.all(), function roomCreated(err,room){
+  		if(err){ 
+  			console.log(err); 
+  			return res.redirect('/')
+  		}
+  		res.redirect('/room/show/' + room.id);
+  	});
+  },
+
+  'show': function(req,res){
+  	Room.findOne(req.param('id'), function foundRoom(err,room){
+  		if(err) return console.log(err);
+  		if(!room) return res.redirect('/');
+  		res.view({
+  			room: room
+  		});
+  	});
+  },
 
 };
